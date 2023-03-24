3cdb879f45d91ebca98e5cb6d47b6be6b6dbea65
Lenakot <ekotova18@mail.ru>
2023-03-24 17:41:13 +0100

    Moved the Search comp to Houses List comp.

--- src/Views/Index.vue
+++ src/Views/Index.vue
@@ -1,11 +1,9 @@
 <template>
-    <HousesListHeader/>
-    <SearchAndSort/>
+    <HousesListHeader />
     <HousesList />
 </template>
 
 <script setup>
 import HousesListHeader from '@/components/HousesListHeader.vue'
-import SearchAndSort from '@/components/SearchAndSort.vue'
 import HousesList from '@/components/HousesList.vue'
 </script>
