41af64c330fe45b98f3dc1ea7121d7eebf1ea62e
Lenakot <ekotova18@mail.ru>
2023-03-24 01:01:31 +0100

    fetch houses list once.  Dislay houses from store

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <Header/>
-  <div class="container">
-     <router-view />
+  <Header />
+  <div class="container" v-if="loaded">
+    <router-view />
     <!-- <ListingForm/> -->
     <!-- <SearchAndSort/> -->
   </div>
@@ -10,10 +10,16 @@
 <script setup>
 import Header from '@/components/Layout/Header.vue'
 import SearchAndSort from '@/components/SearchAndSort.vue'
+import { useHousesStore } from '@/stores/houseStore.js'
+import { onBeforeMount, ref } from 'vue';
+const housesStore = useHousesStore()
+const loaded = ref(false)
 
-
+onBeforeMount(async () => {
+  await housesStore.getHousesList()
+  loaded.value = true
+})
 </script>
-
 
 <style lang='scss' scoped>
 @import './assets/styles/global.scss'
--- src/components/HousesList.vue
+++ src/components/HousesList.vue
@@ -1,17 +1,10 @@
 <template>
-    <House v-for="house of houses" :key='house.id' :house="house" />
+    <House v-for="house of housesStore.listOfHouses" :key='house.id' :house="house" />
 </template>
 
 <script setup>
 import House from '@/components/House.vue'
 import { useHousesStore } from '@/stores/houseStore.js'
-import { ref, onMounted } from 'vue';
-
 const housesStore = useHousesStore()
-const houses = ref([])
-
-onMounted(async () => {
-    houses.value = await housesStore.getHousesList()
-})
 
 </script>
