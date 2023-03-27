78c29e52fe5fcdb9a7e018f55e36c5ffad54da4b
Lenakot <ekotova18@mail.ru>
2023-03-27 22:45:46 +0200

    Added Main Page comp. Added a filter based on search, size or price

---
+++ src/components/MainPage.vue
@@ -0,0 +1,257 @@
+<template>
+    <div class="main-page-wrapper">
+        <div class="header-wrapper">
+            <div class="header">
+                <div class="header-title">Houses</div>
+                <router-link v-if="isMobile" to="/create-new-listing" class="header-create-list">
+                    <img class="header-create-list-image" src="/png/ic_plus_grey@3x.png" alt="plus">
+                </router-link>
+                <router-link v-else to="/create-new-listing" class="header-create-list">
+                    <img class="header-create-list-image" src="/png/ic_plus_white@3x.png" alt="plus"><span
+                        class="header-create-list-text">CREATE NEW</span>
+                </router-link>
+            </div>
+            <div class="search-and-sort">
+                <form class="search-wrapper" action="#">
+                    <img class='search-wrapper-image' src="/png/ic_search@3x.png" alt="search">
+                    <input class='search-wrapper-input' @click="clearInput()" type="text" placeholder="Search for a house"
+                        name="search" v-model="housesStore.search" ref="search">
+                </form>
+                <div class="sort-wrapper">
+                    <div @click='sortByPrice' class="sort-wrapper-price sort-wrapper-title">Price</div>
+                    <div @click='sortBySize' class="sort-wrapper-size sort-wrapper-title">Size</div>
+                </div>
+            </div>
+            <div v-if="housesStore.search != ''" class="found">Found: {{ housesStore.filter.length }}</div>
+            <div v-if='housesStore.filter.length == 0' class='search-undefined'>
+                <img src="/png/img_empty_houses@3x.png" alt="not found" class='search-undefined-image'>
+                <div class='search-undefined-description'>No results found.</div>
+                <div class='search-undefined-description'>Please try another keyword.</div>
+            </div>
+        </div>
+        <House v-for="house of housesStore.filter" :key='house.id' :house="house" />
+    </div>
+</template>
+
+<script setup>
+import House from '@/components/House.vue'
+import { useHousesStore } from '@/stores/houseStore.js'
+import { ref, onMounted, onUnmounted } from 'vue';
+const housesStore = useHousesStore()
+
+function clearInput() {
+    housesStore.search = ''
+}
+
+function sortByPrice() {
+    housesStore.sortParam = 'price'
+}
+function sortBySize() {
+    housesStore.sortParam = 'size'
+}
+
+const isMobile = ref(false);
+const checkIfMobile = () => {
+    isMobile.value = window.innerWidth < 768;
+}
+
+onMounted(async () => {
+    window.addEventListener("resize", checkIfMobile);
+    checkIfMobile();
+});
+
+onUnmounted(async () => {
+    window.removeEventListener("resize", checkIfMobile);
+});
+</script>
+
+<style lang="scss" scoped>
+.main-page-wrapper {
+    margin: 40px 300px;
+
+    @media screen and (max-width: 1150px) {
+        margin: 40px 150px;
+    }
+
+    @media screen and (max-width: 767px) {
+        margin: 30px;
+    }
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &-title {
+        font-family: var(--montserrat);
+        font-size: var(--header-1-desktop);
+        font-weight: var(--bold);
+        color: var(--text-primary);
+
+        @media screen and (max-width: 767px) {
+            flex-basis: 100%;
+            text-align: center;
+            font-size: var(--header-1-mobile);
+        }
+    }
+
+    &-create-list {
+        background: var(--primary);
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        padding: 10px 20px;
+        height: 40px;
+        text-decoration: none;
+        cursor: pointer;
+
+        @media screen and (max-width: 767px) {
+            padding: 0;
+            background: none;
+        }
+
+
+        &-text {
+            color: var(--background2);
+            font-family: var(--montserrat);
+            font-weight: var(--bold);
+            font-size: var(----buttons-n-tabs-desktop);
+        }
+
+        &-image {
+            object-fit: cover;
+            width: 20px;
+            height: 20px;
+        }
+    }
+}
+
+.search-and-sort {
+    display: flex;
+    justify-content: space-between;
+    margin: 20px 0;
+    gap: 20px;
+
+    @media screen and (max-width: 767px) {
+        display: flex;
+        flex-direction: column;
+    }
+}
+
+.search-wrapper {
+    width: 400px;
+    height: 40px;
+    padding: 8px 20px;
+    border-radius: 10px;
+    background: var(--tertiary);
+    display: flex;
+    gap: 10px;
+
+    @media screen and (max-width: 767px) {
+        width: auto;
+    }
+
+
+    &-image {
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+    }
+
+    &-input {
+        background: var(--tertiary);
+        width: inherit;
+        border: none;
+        font-family: var(--open-sans);
+        font-size: var(--input-title-desktop);
+        font-weight: var(--regular);
+        color: var(--text-primary);
+        outline: none;
+
+        @media screen and (max-width: 767px) {
+            font-size: var(--input-title-mobile);
+        }
+    }
+}
+
+.sort-wrapper {
+    width: 300px;
+    height: 40px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    @media screen and (max-width: 767px) {
+        width: auto;
+    }
+
+    &-price {
+        background: var(--primary);
+        border-top-left-radius: 10px;
+        border-end-start-radius: 10px;
+    }
+
+    &-size {
+        background: var(--quaternary);
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+    }
+
+    &-title {
+        color: var(--background2);
+        font-family: var(--montserrat);
+        font-weight: var(--bold);
+        font-size: var(----buttons-n-tabs-desktop);
+        text-align: center;
+        padding: 8px;
+
+        @media screen and (max-width: 767px) {
+            font-size: var(----buttons-n-tabs-mobile);
+            padding: 13px;
+        }
+    }
+}
+
+.search-undefined {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    margin: 20px 0;
+    height: 400px;
+    width: auto;
+
+    &-image {
+        width: auto;
+        height: inherit;
+        padding: 50px;
+        object-fit: contain;
+    }
+
+    &-description {
+        font-family: var(--open-sans);
+        font-size: var(--body-text-desktop);
+        color: var(--text-secondary);
+        font-weight: var(--regular);
+        text-align: center;
+
+        @media screen and (max-width: 767px) {
+            font-size: var(--body-text-mobile);
+            padding: 13px;
+        }
+    }
+}
+
+.found {
+    font-family: var(--open-sans);
+    font-size: var(--body-text-desktop);
+    color: var(--text-secondary);
+    font-weight: var(--regular);
+
+    @media screen and (max-width: 767px) {
+        font-size: var(--body-text-mobile);
+    }
+
+}
+</style>
--- src/stores/houseStore.js
+++ src/stores/houseStore.js
@@ -3,91 +3,86 @@ import api from "@/api.js";
 import { getGlobalThis } from "@vue/shared";
 
 export const useHousesStore = defineStore("housesStore", {
-  state: () => ({
-    listOfHouses: [],
-    search: "",
-  }),
-  getters: {
-    getHouseById: (state) => {
-      return (id) =>
-        state.listOfHouses.find((house) => {
-          if (house.id == id) {
-            return house;
-          }
-        });
-    },
-    filter: (state) => {
-      return state.listOfHouses.filter((house) => {
-        if (
-          house.location.city
-            .toLowerCase()
-            .includes(state.search.toLowerCase()) ||
-          house.location.street
-            .toLowerCase()
-            .includes(state.search.toLowerCase())
-        ) {
-          return house;
-        }
-      });
-    },
-    sortByPrice: (state) => {
-      return state.listOfHouses.sort(
-        (prevHouse, currHouse) => prevHouse.price - currHouse.price
-      );
-    },
-    sortBySize: (state) => {
-      return state.listOfHouses.sort(
-        (prevHouse, currHouse) => prevHouse.size - currHouse.size
-      );
-    },
-    //Give max 3 reccomendation based on same city
-    getReccomendations: (state) => {
-      return (city, id) => {
-        // house.id != id -  exclude the same house from the reccomendation list});
-        const reccomendationList = state.listOfHouses.filter(
-          (house) => house.location.city == city && house.id != id
-        );
-        return reccomendationList.slice(0, 3);
-      };
-    },
-  },
+    state: () => ({
+        listOfHouses: [],
+        search: "",
+        sortParam: "",
+    }),
+    getters: {
+        getHouseById: (state) => {
+            return (id) =>
+                state.listOfHouses.find((house) => {
+                    if (house.id == id) {
+                        return house;
+                    }
+                });
+        },
+        filter() {
+            const filteredHouses = this.listOfHouses.filter(
+                (house) =>
+                    house.location.city
+                        .toLowerCase()
+                        .includes(this.search.toLowerCase()) ||
+                    house.location.street
+                        .toLowerCase()
+                        .includes(this.search.toLowerCase())
+            );
 
-  actions: {
-    async getHousesList() {
-      this.listOfHouses = await api.getListHouses();
-      console.log("houses refreshed");
-      return this.listOfHouses;
+            if (this.sortParam == "") {
+                return filteredHouses;
+            } else {
+                return this.sort(filteredHouses);
+            }
+        },
+        sort: (state) => {
+            return (listToSort = state.listOfHouses) => {
+                if (state.sortParam == "price") {
+                    return listToSort.sort(
+                        (prevHouse, currHouse) => prevHouse.price - currHouse.price
+                    );
+                } else if (state.sortParam == "size") {
+                    return listToSort.sort(
+                        (prevHouse, currHouse) => prevHouse.size - currHouse.size
+                    );
+                }
+            };
+        },
+        //Give max 3 reccomendation based on same city
+        getRecommendations: (state) => {
+            return (city, id) => {
+                const reccomendationList = state.listOfHouses.filter(
+                    (house) => house.location.city == city && house.id != id
+                );
+                return reccomendationList.slice(0, 3);
+            };
+        },
     },
-    async deleteHouse(id) {
-      await api.deleteHouse(id);
 
-      //  Necessary to remove the house from the main page immediately because a request for a list of houses is only sent once.
-      for (let house of this.listOfHouses) {
-        if (house.id === id) {
-          this.listOfHouses.splice(this.listOfHouses.indexOf(house), 1);
-        }
-      }
+    actions: {
+        async getHousesList() {
+            this.listOfHouses = await api.getListHouses();
+            return this.listOfHouses;
+        },
+        async deleteHouse(id) {
+            await api.deleteHouse(id);
+            for (let house of this.listOfHouses) {
+                if (house.id === id) {
+                    this.listOfHouses.splice(this.listOfHouses.indexOf(house), 1);
+                }
+            }
+        },
+        async createNewHouse(house, image) {
+            const newHouse = await api.createHouse(house);
+            await api.uploadImage(newHouse.id, image);
+            await this.getHousesList();
+        },
+        async editHouse(houseId, house, image) {
+            await api.editHouse(houseId, house);
+            if (image !== null) {
+                await api.uploadImage(houseId, image);
+            }
+            await this.getHousesList();
+            console.log("edit end");
+        },
     },
-    async createNewHouse(house, image) {
-      const newHouse = await api.createHouse(house);
-      await api.uploadImage(newHouse.id, image);
-      await this.getHousesList();
-      // const houseToAdd = this.getHouseById(newHouse.id);
-      // // this.listOfHouses[this.listOfHouses.length] = newHouseFullInfo;
-      // console.log("after-", houseToAdd);
-      // // this.listOfHouses[this.listOfHouses.length].image = image;
-      // console.log("after-", houseToAdd);
-    },
-    async editHouse(houseId, house, image) {
-      await api.editHouse(houseId, house);
-      if (image !== null) {
-        await api.uploadImage(houseId, image);
-      }
-      await this.getHousesList();
-      console.log("edit end");
-    },
-    changeSearchField() {
-      // this.search =
-    },
-  },
 });
