ecac6133d7f951dc6a0d5404ced8afb951b85d5c
Lenakot <ekotova18@mail.ru>
2023-03-29 01:17:55 +0200

    Added preloader

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,30 @@
     <Header />
     <router-view />
   </div>
+  <div v-else-if="error">
+    Something went wrong. Reload the page and try again later.
+  </div>
+  <Preloader v-else />
 </template>
 
 <script setup>
 import Header from '@/components/layout/Header.vue'
+import Preloader from '@/components//Preloader.vue'
 import { useHousesStore } from '@/stores/houseStore.js'
 import { onBeforeMount, ref } from 'vue'
 const housesStore = useHousesStore()
 const loaded = ref(false)
+const error = ref(false)
 
 onBeforeMount(async () => {
-  await housesStore.getHousesList()
-  loaded.value = true
+  try {
+    await housesStore.getHousesList()
+    loaded.value = true
+  } catch (e) {
+    error.value = true
+  }
 })
+
 </script>
 
 <style lang='scss' scoped>
---
+++ src/components/Preloader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="loader-wrap">
+        <div class="loader"></div>
+    </div>
+</template>
+
+<style scoped>
+.loader-wrap {
+    display: flex;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+}
+
+.loader {
+    border: 20px solid #EAF0F6;
+    border-radius: 50%;
+    border-top: 20px solid var(--primary);
+    width: 200px;
+    height: 200px;
+    animation: spinner 4s linear infinite;
+}
+
+@keyframes spinner {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
